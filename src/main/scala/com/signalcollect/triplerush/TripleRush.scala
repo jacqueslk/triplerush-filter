1d0f9242af2361a73210cfb513f43eb72a1354f7
jacqueslk <jacqueslk@users.noreply.github.com>
2014-10-20 11:55:17 +0200

    Add change from commit #67938d6 in triplerush main

--- src/main/scala/com/signalcollect/triplerush/TripleRush.scala
+++ src/main/scala/com/signalcollect/triplerush/TripleRush.scala
@@ -138,7 +138,7 @@ case class TripleRush(
 
   def prepareExecution {
     graph.awaitIdle
-    graph.execute(ExecutionConfiguration.withExecutionMode(ExecutionMode.ContinuousAsynchronous))
+    graph.execute(ExecutionConfiguration().withExecutionMode(ExecutionMode.ContinuousAsynchronous))
     graph.awaitIdle
     canExecute = true
     optimizer = optimizerCreator(this)
